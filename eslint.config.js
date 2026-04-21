3d2696ce2b49e7d8546d53478426ab96912d4c07
Linh Tran <li415173@ucf.edu>
2026-04-21 14:27:57 -0400

    Enhance ESLint config with additional globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,21 @@
 import js from "@eslint/js";
 
 export default [
-  js.configs.recommended
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        process: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        fetch: "readonly"
+      }
+    }
+  }
 ];
